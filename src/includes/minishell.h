565d999b5dceed3bd93e1bbd500d65c5298c9092
Kanata Isobe <kaisobe@c6r2s6.42tokyo.jp>
2025-02-06 11:57:37 +0900

    implement SIGQUIT

--- src/includes/minishell.h
+++ src/includes/minishell.h
@@ -18,7 +18,7 @@
 # define BUFFER_SIZE 1000
 # define ASCII_SIZE 128
 
-extern int				g_signal;
+extern int					g_signal;
 
 typedef enum e_token_type
 {
@@ -35,22 +35,22 @@ typedef enum e_token_type
 	TK_REDIRECT_OUT_APPEND,
 	TK_REDIRECT_IN = '<',
 	TK_UNDEFINED,
-}						t_token_type;
+}							t_token_type;
 
 typedef struct s_token
 {
-	t_token_type		type;
-	char				*data;
-	struct s_token		*prev;
-	struct s_token		*next;
-}						t_token;
+	t_token_type			type;
+	char					*data;
+	struct s_token			*prev;
+	struct s_token			*next;
+}							t_token;
 
 typedef enum e_lexer_status
 {
 	IN_SINGLE_QAOT = '\'',
 	IN_DOUBLE_QUOT = '\"',
 	NORMAL = '\0',
-}						t_lexer_status;
+}							t_lexer_status;
 
 typedef enum e_astnode_type
 {
@@ -59,123 +59,122 @@ typedef enum e_astnode_type
 	ASTND_AND,
 	ASTND_CMD,
 	ASTND_UNDEFINED,
-}						t_astnode_type;
+}							t_astnode_type;
 
 typedef enum e_ex_astnode_type
 {
 	EX_ASTND_OR,
 	EX_ASTND_AND,
 	EX_ASTND_PIPETOP,
-}						t_ex_astnode_type;
-
+}							t_ex_astnode_type;
+typedef t_token				t_redirect;
+typedef t_token				t_arg;
+typedef t_token				t_cmd;
+typedef unsigned char		t_status;
+typedef struct sigaction	t_sigaction;
 typedef struct s_ex_astnode
 {
-	t_ex_astnode_type	type;
-	t_list				*cmds;
-	struct s_ex_astnode	*left;
-	struct s_ex_astnode	*right;
-}						t_ex_astnode;
-
-typedef t_token			t_redirect;
-typedef t_token			t_arg;
-typedef t_token			t_cmd;
-typedef void			*(*t_fvoid)(void *, ...);
+	t_ex_astnode_type		type;
+	t_list					*cmds;
+	struct s_ex_astnode		*left;
+	struct s_ex_astnode		*right;
+}							t_ex_astnode;
 
 typedef struct s_astnode
 {
-	t_astnode_type		type;
-	t_cmd				*cmd;
-	int					is_last_cmd;
-	int					is_first_cmd;
-	char				**arg_strs;
-	t_arg				*args;
-	t_redirect			*redirects;
-	size_t				arg_cnt;
-	struct s_astnode	*left;
-	struct s_astnode	*right;
-}						t_astnode;
+	t_astnode_type			type;
+	t_cmd					*cmd;
+	int						is_last_cmd;
+	int						is_first_cmd;
+	char					**arg_strs;
+	t_arg					*args;
+	t_redirect				*redirects;
+	size_t					arg_cnt;
+	struct s_astnode		*left;
+	struct s_astnode		*right;
+}							t_astnode;
 
 typedef struct s_pids
 {
-	pid_t				pid;
-	struct s_pids		*next;
-}						t_pids;
+	pid_t					pid;
+	struct s_pids			*next;
+}							t_pids;
 
 typedef struct s_all
 {
-	char				*line;
-	char				**env;
-	t_token				*tokens;
-	t_astnode			*tree;
-	t_ex_astnode		*ex_tree;
-}						t_all;
-
-typedef unsigned char	t_status;
+	char					*line;
+	char					**env;
+	t_token					*tokens;
+	t_astnode				*tree;
+	t_ex_astnode			*ex_tree;
+}							t_all;
 
 typedef struct s_env
 {
-	char				*value;
-	struct s_env		*next;
-}						t_env;
+	char					*value;
+	struct s_env			*next;
+}							t_env;
 
-int						builtin_env(char **envp);
-int						builtin_pwd(void);
-int						builtin_unset(int ac, char *av[], char **envp);
-int						builtin_echo(int ac, char *av[]);
-int						builtin_exit(int argc, char *argv[], int last_status);
+int							builtin_env(char **envp);
+int							builtin_pwd(void);
+int							builtin_unset(int ac, char *av[], char **envp);
+int							builtin_echo(int ac, char *av[]);
+int							builtin_exit(int argc, char *argv[],
+								int last_status);
 
-void					exec_heredoc(t_astnode *node);
-pid_t					fork_and_exec_child(t_astnode *node, int old_pipes[2],
-							int new_pipes[2]);
-t_status				executer(t_ex_astnode *root);
-void					expander(t_astnode *node);
-char					**grobal_env(int get_or_set, ...);
-t_token					*lexer(char *line);
-t_token					*new_token(t_token_type type, char *data);
-t_token					*last_token(t_token *token);
-void					addback_token(t_token **token, t_token *newtoken);
-void					addfront_token(t_token **token, t_token *newtoken);
-t_token					*head_token(t_token *token);
-size_t					size_token(t_token *token);
-void					delone_token(t_token *token, void (*del)(void *));
-void					cut_token(t_token **token, t_token *to_delete);
-void					print_tokens(t_token *token);
-void					print_token(t_token *token);
-int						tokenizer(t_token **token_ptr);
-t_astnode				*new_astnode(void);
-void					add_astnode(t_astnode **root, t_astnode *left,
-							t_astnode *right);
-void					print_tree(t_astnode *root);
-t_astnode				*parser(t_token *token);
-int						is_directory(char *str);
-int						is_command(char *str);
-char					*replace_env_vars(char *str, char **env);
-char					*replace_env_vars_quate(char *str, char **env);
-void					free_astnode(t_astnode *root);
-void					free_tokens(t_token *token);
-void					free_asttree(t_astnode *root);
-t_astnode				*parse_or_and(t_token **token);
-char					*grobal_tmpfile(int get_or_set, ...);
-void					check_fds(t_astnode *tree);
-void					read_and_print(void);
-t_pids					*new_pids(pid_t pid);
-void					print_pids(t_pids *pids);
-t_pids					*last_pid(t_pids *lst);
-void					addback_pid(t_pids **lst, t_pids *newpid);
-char					**create_args(t_astnode *node);
-void					set_arginfo(t_astnode *node);
-t_ex_astnode			*semantic_analyzer(t_astnode *root);
-void					print_cmd(t_astnode *cmd_node, int depth);
-void					print_ex_tree(t_ex_astnode *root);
-t_ex_astnode			*new_ex_astnode(t_ex_astnode_type type);
-pid_t					*create_pids(int n);
-int						**create_pipes(int n);
-void					free_ex_asttree(t_ex_astnode *node);
-int						builtin_pwd(void);
-char					*get_shell_prompt(int exist_nl);
-char					*trim_space(char *line);
-t_status				grobal_status(int get_or_set, ...);
-t_status				builtin(char *av[]);
-int						is_builtin(char *cmd);
+void						exec_heredoc(t_astnode *node);
+pid_t						fork_and_exec_child(t_astnode *node,
+								int old_pipes[2], int new_pipes[2]);
+t_status					executer(t_ex_astnode *root);
+void						expander(t_astnode *node);
+char						**grobal_env(int get_or_set, ...);
+t_token						*lexer(char *line);
+t_token						*new_token(t_token_type type, char *data);
+t_token						*last_token(t_token *token);
+void						addback_token(t_token **token, t_token *newtoken);
+void						addfront_token(t_token **token, t_token *newtoken);
+t_token						*head_token(t_token *token);
+size_t						size_token(t_token *token);
+void						delone_token(t_token *token, void (*del)(void *));
+void						cut_token(t_token **token, t_token *to_delete);
+void						print_tokens(t_token *token);
+void						print_token(t_token *token);
+int							tokenizer(t_token **token_ptr);
+t_astnode					*new_astnode(void);
+void						add_astnode(t_astnode **root, t_astnode *left,
+								t_astnode *right);
+void						print_tree(t_astnode *root);
+t_astnode					*parser(t_token *token);
+int							is_directory(char *str);
+int							is_command(char *str);
+char						*replace_env_vars(char *str, char **env);
+char						*replace_env_vars_quate(char *str, char **env);
+void						free_astnode(t_astnode *root);
+void						free_tokens(t_token *token);
+void						free_asttree(t_astnode *root);
+t_astnode					*parse_or_and(t_token **token);
+char						*grobal_tmpfile(int get_or_set, ...);
+void						check_fds(t_astnode *tree);
+void						read_and_print(void);
+t_pids						*new_pids(pid_t pid);
+void						print_pids(t_pids *pids);
+t_pids						*last_pid(t_pids *lst);
+void						addback_pid(t_pids **lst, t_pids *newpid);
+char						**create_args(t_astnode *node);
+void						set_arginfo(t_astnode *node);
+t_ex_astnode				*semantic_analyzer(t_astnode *root);
+void						print_cmd(t_astnode *cmd_node, int depth);
+void						print_ex_tree(t_ex_astnode *root);
+t_ex_astnode				*new_ex_astnode(t_ex_astnode_type type);
+pid_t						*create_pids(int n);
+int							**create_pipes(int n);
+void						free_ex_asttree(t_ex_astnode *node);
+int							builtin_pwd(void);
+char						*get_shell_prompt(int exist_nl);
+char						*trim_space(char *line);
+t_status					grobal_status(int get_or_set, ...);
+t_status					builtin(char *av[]);
+int							is_builtin(char *cmd);
+void						set_signal(void);
 
 #endif
