34f19fbab7e7f21e4ec935286ed95a18175edc7e
Kanata Isobe <kaisobe@c6r2s5.42tokyo.jp>
2025-02-07 15:56:12 +0900

    implement env var command

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -79,30 +79,115 @@ char	*replace_env_vars_quate(char *str, char **env)
 	return (out);
 }
 
+void	addback_tokens(t_token **token, t_token *new)
+{
+	t_token	*last_node;
+
+	if (!new)
+		return ;
+	last_node = last_token(*token);
+	new->prev = last_node;
+	if (last_node == NULL)
+	{
+		*token = new;
+		return ;
+	}
+	last_node->next = new;
+	return ;
+}
+
+void	insert_token(t_token **dst, t_token *src)
+{
+	t_token	*last;
+	t_token	*next;
+
+	if (*dst)
+	{
+		*dst = src;
+		return ;
+	}
+	next = (*dst)->next;
+	(*dst)->next = src;
+	if (src)
+	{
+		src->prev = (*dst);
+	}
+	last = last_token(src);
+	if (last)
+	{
+		last->next = next;
+	}
+	if (next)
+	{
+		next->prev = last;
+	}
+	return ;
+}
+
+t_token	*token_variable_split(t_token **token)
+{
+	char	*expanded;
+	char	**splited;
+	t_token	*new_tokens;
+	t_token	*new;
+	size_t	i;
+
+	if (!(*token && (*token)->type == TK_VALIABLE))
+		return (NULL);
+	expanded = replace_env_vars_quate((*token)->data, grobal_env(GET));
+	splited = ft_split(expanded, ' ');
+	free(expanded);
+	if (!splited)
+		return (NULL);
+	i = 0;
+	new_tokens = NULL;
+	while (splited[i])
+	{
+		new = new_token(TK_WORD, splited[i]);
+		addback_token(&new_tokens, new);
+		i++;
+	}
+	return (new_tokens);
+}
+
 void	expander(t_astnode *node)
 {
 	t_token	*token;
 	char	*tmp;
 	size_t	cnt;
+	t_token	*new_tokens;
+	t_token	*to_add;
 
 	cnt = 0;
 	if (!node)
 		return ;
-	token = node->cmd;
-	while (cnt < 3)
+	token = node->args;
+	node->args = NULL;
+	while (cnt < 2)
 	{
 		while (token)
 		{
-			tmp = token->data;
-			token->data = replace_env_vars_quate(tmp, grobal_env(GET));
-			token = token->next;
-			free(tmp);
+			if (token->type == TK_VALIABLE)
+			{
+				new_tokens = token_variable_split(&token);
+				addback_tokens(&node->args, new_tokens);
+				token = token->next;
+			}
+			else
+			{
+				tmp = token->data;
+				token->data = replace_env_vars_quate(tmp, grobal_env(GET));
+				to_add = token;
+				token = token->next;
+				addback_token(&node->args, to_add);
+				free(tmp);
+			}
 		}
 		cnt++;
 		if (cnt == 1)
+		{
 			token = node->redirects;
-		else
-			token = node->args;
+		}
 	}
 	expander(node->left);
 	expander(node->right);
