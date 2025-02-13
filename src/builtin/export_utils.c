418896027522de49a557300a8e0971a687e2a105
Temma Kitago <tkitago@c6r2s10.42tokyo.jp>
2025-02-13 11:39:47 +0900

    exportのエラー処理修正

--- src/builtin/export_utils.c
+++ src/builtin/export_utils.c
@@ -3,20 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   export_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaisobe <kaisobe@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tkitago <tkitago@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 20:32:51 by tkitago           #+#    #+#             */
-/*   Updated: 2025/02/11 08:28:19 by kaisobe          ###   ########.fr       */
+/*   Updated: 2025/02/13 11:34:09 by tkitago          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	check_export_no_equal(char *line, t_env **env_lst)
+static int	valid_export(char *key)
 {
-	if (!find_env(*env_lst, line))
+	int	i;
+
+	i = 0;
+	if (!key)
+		return (1);
+	while (key[i])
 	{
-		append_env_noequal(env_lst, line, "");
+		if (ft_isalnum(key[i]) || key[i] == '_')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
+int	check_export_no_equal(char *key, t_env **env_lst)
+{
+	if (valid_export(key))
+	{
+		printf("export: `%s': not a valid identifier\n", key);
+		return (1);
+	}
+	if (!find_env(*env_lst, key))
+	{
+		append_env_noequal(env_lst, key, "");
 	}
 	return (0);
 }
@@ -25,17 +47,20 @@ int	check_export_line(char *line, t_env **env_lst)
 {
 	char	*check_plus;
 
-	if (!ft_isalpha(line[0]) || line[0] == '_')
+	if ((ft_isalpha(line[0]) || line[0] == '_'))
+	{
+		check_plus = ft_strstr(line, "+=");
+		if (check_plus)
+			return (check_export_plus(line, check_plus, env_lst));
+		else if (ft_strchr(line, '='))
+			return (check_export_equal(line, env_lst));
+		return (check_export_no_equal(line, env_lst));
+	}
+	else
 	{
 		printf("export: `%s': not a valid identifier\n", line);
 		return (1);
 	}
-	check_plus = ft_strstr(line, "+=");
-	if (check_plus)
-		return (check_export_plus(line, check_plus, env_lst));
-	else if (ft_strchr(line, '='))
-		return (check_export_equal(line, env_lst));
-	return (check_export_no_equal(line, env_lst));
 }
 
 int	check_export_plus(char *line, char *check_plus, t_env **env_lst)
@@ -43,13 +68,14 @@ int	check_export_plus(char *line, char *check_plus, t_env **env_lst)
 	char	*key;
 	char	*value;
 
-	if (!ft_isalnum(*(check_plus - 1)))
-	{
-		printf("export: `%s': not a valid identifier\n", line);
-		return (1);
-	}
 	key = extract_key_plus(line);
 	value = extract_value(line);
+	if (!ft_isalnum(*(check_plus - 1)) || valid_export(key))
+	{
+		printf("export: `%s': not a valid identifier\n", line);
+		free(key);
+		return (1);
+	}
 	if (!find_env(*env_lst, key))
 		append_env(env_lst, key, value);
 	else
@@ -66,6 +92,12 @@ int	check_export_equal(char *line, t_env **env_lst)
 
 	key = extract_key(line);
 	value = extract_value(line);
+	if (valid_export(key))
+	{
+		printf("export: `%s': not a valid identifier\n", line);
+		free(key);
+		return (1);
+	}
 	util_setenv(env_lst, key, value, 0);
 	free(key);
 	free(value);
